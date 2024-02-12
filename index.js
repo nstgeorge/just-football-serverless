007e46ece6142517b71c11bb74bcccaeae055615
Nate St. George <natestgeorge@gmail.com>
2024-02-11 20:18:54 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const transformResponseToObject = async (res) => ({
+  statusCode: res.status,
+  headers: res.headers,
+  body: JSON.stringify(!res.ok ? { message: await res.text() } : await res.json())
+})
+
+/**
+ * Handle all HTTP requests. All paths are proxied here.
+ */
+module.exports.handler = async (event) => {
+  if (event.rawPath.length > 1) {
+    const path = event.rawPath.startsWith('/') ? event.rawPath.slice(1) : event.rawPath
+    try {
+      const response = await fetch(`${process.env.CFB_API_BASE_URL}${path}?${event.rawQueryString}`, {
+        headers: {
+          Authorization: `Bearer ${process.env.CFB_API_TOKEN}`
+        }
+      })
+      return await transformResponseToObject(response)
+    } catch (e) {
+      console.log('An error occurred:', e.message)
+      return { statusCode: 500, body: JSON.stringify({ err: 'An unexpected error occurred while proxying the request.' }) }
+    }
+  } else {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(
+        {
+          message: "Path required."
+        }
+      )
+    }
+  }
+}
