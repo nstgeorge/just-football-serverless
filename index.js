79b53ad38626cfdc2d1a5a9e31246720636ddc76
Nate St. George <natestgeorge@gmail.com>
2024-02-12 20:03:29 -0700

    Add CORS header

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const transformResponseToObject = async (res) => ({
   statusCode: res.status,
-  headers: res.headers,
+  headers: {
+    'Access-Control-Allow-Origin': '*', // TODO: Put something real here
+    ...res.headers
+  },
   body: JSON.stringify(!res.ok ? { message: await res.text() } : await res.json())
 })
 
@@ -12,9 +15,6 @@ module.exports.handler = async (event) => {
     const path = event.rawPath.startsWith('/') ? event.rawPath.slice(1) : event.rawPath
     try {
       const response = await fetch(`${process.env.CFB_API_BASE_URL}${path}?${event.rawQueryString}`, {
-        headers: {
-          Authorization: `Bearer ${process.env.CFB_API_TOKEN}`
-        }
       })
       return await transformResponseToObject(response)
     } catch (e) {
